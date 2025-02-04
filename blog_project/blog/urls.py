e8b79d7916e20d4cd4db95db3e9c57e7f0187382
= <=>
2025-02-04 11:15:08 +0530

    first commit

---
+++ blog_project/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('create/', views.create_post, name='create_post'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('logout/', views.logout_user, name='logout'),
+]
---
+++ blog_project/blog/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .models import BlogPost
+from .forms import BlogPostForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+def home(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if request.user != post.author:
+        return redirect('home')
+    
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if request.user != post.author:
+        return redirect('home')
+
+    if request.method == "POST":
+        post.delete()
+        return redirect('home')
+
+    return render(request, 'blog/delete_post.html', {'post': post})
+
+@login_required
+def post_detail(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def logout_user(request):
+    logout(request)  # This will log the user out and clear their session
+    return redirect('home') 
